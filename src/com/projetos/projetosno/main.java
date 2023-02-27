258656bed86717df3893fbd6e29b25caf4b12676
Rogerio Alves <sigmamix10@gmail.com>
2023-02-27 13:16:45 -0300

    Aulas de Listas Encadeadas 01

--- src/com/projetos/projetosno/main.java
+++ src/com/projetos/projetosno/main.java
@@ -1,2 +1,27 @@
-package com.projetos.projetosno;public class main {
+package com.projetos.projetosno;
+
+public class main {
+    public static void main(String[] args) {
+        No<String> no1 = new No<>("Conteúdo no1");
+        No<String> no2 = new No<>("Conteúdo no2");
+
+        no1.setProximoNo(no2);
+
+        No<String> no3 = new No<>("Conteúdo no3");
+        no2.setProximoNo(no3);
+
+        No<String> no4 = new No<>("Conteúdo no4");
+        no3.setProximoNo(no4);
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+        System.out.println("--------------------");
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+
+    }
 }
